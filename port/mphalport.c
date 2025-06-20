ed1aa768c518a71de08b3d29c413e60884343abb
fibonacci <211732944+fibonacci61@users.noreply.github.com>
2025-06-20 09:59:46 +0300

    fix: write to serial channel 1 instead of 0

--- port/mphalport.c
+++ port/mphalport.c
@@ -5,5 +5,5 @@
 #include "sdk.h"
 
 void mp_hal_stdout_tx_strn_cooked(const char* str, mp_uint_t len) {
-	vexSerialWriteBuffer(0, (const uint8_t*)str, len);
+	vexSerialWriteBuffer(1, (const uint8_t*)str, len);
 }
