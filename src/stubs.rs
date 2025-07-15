e38ae31d212386400f044a5b8d4bfec04bdf5f29
fibonacci <211732944+fibonacci61@users.noreply.github.com>
2025-07-15 13:12:54 +0300

    feat: add proper libc stubs

--- src/stubs.rs
+++ src/stubs.rs
@@ -1,28 +1,117 @@
-// TODO: use the proper function signatures
+#![allow(non_camel_case_types)]
 
-macro_rules! stub {
-    ($($name:ident),*) => {
-        $(
-            #[unsafe(no_mangle)]
-            extern "C" fn $name() {}
-        )*
-    }
+use cty::{c_int, c_long, c_void, ptrdiff_t, size_t, ssize_t};
+use no_std_io::io::{Read, Write};
+
+use crate::{
+    exit,
+    serial::{STDIN, STDOUT},
+};
+
+const STDIN_FILENO: c_int = 0;
+const STDOUT_FILENO: c_int = 1;
+const STDERR_FILENO: c_int = 2;
+
+unsafe extern "C" {
+    static mut errno: c_int;
 }
 
-#[rustfmt::skip]
-stub!(
-    // called by __libc_init_array; unneeded
-    _init,
-    // called by __libc_fini_array; unneeded
-    _fini,
-    _sbrk,
-    _write,
-    _read,
-    _lseek,
-    _close,
-    _fstat,
-    _isatty,
-    _exit,
-    _kill,
-    _getpid
-);
+const EIO: c_int = 5;
+const EBADF: c_int = 9;
+const ENOMEM: c_int = 12;
+const ENOSYS: c_int = 88;
+
+type off_t = c_long;
+
+#[unsafe(no_mangle)]
+extern "C" fn _init() {}
+
+#[unsafe(no_mangle)]
+extern "C" fn _fini() {}
+
+#[unsafe(no_mangle)]
+extern "C" fn _sbrk(_incr: ptrdiff_t) -> *mut c_void {
+    unsafe { errno = ENOMEM };
+    core::ptr::null_mut()
+}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _read(fd: c_int, buf: *mut c_void, count: size_t) -> ssize_t {
+    if fd != STDIN_FILENO {
+        unsafe { errno = EBADF };
+        return -1;
+    }
+
+    let buf = unsafe { core::slice::from_raw_parts_mut(buf as *mut u8, count) };
+
+    let ret = STDIN
+        .try_lock()
+        .expect("attempt to read from stdin while locked")
+        .read(buf);
+
+    ret.map(|read| read as ssize_t).unwrap_or_else(|_| {
+        unsafe { errno = EIO };
+        -1
+    })
+}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _write(fd: c_int, buf: *const c_void, count: size_t) -> ssize_t {
+    if fd != STDOUT_FILENO && fd != STDERR_FILENO {
+        unsafe { errno = EBADF };
+        return -1;
+    }
+
+    let buf = unsafe { core::slice::from_raw_parts(buf as *const u8, count) };
+
+    let ret = STDOUT
+        .try_lock()
+        .expect("attempt to write to stdout while locked")
+        .write(buf);
+
+    ret.map(|written| written as ssize_t).unwrap_or_else(|_| {
+        unsafe { errno = EIO };
+        -1
+    })
+}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _close(_fd: c_int) -> c_int {
+    unsafe { errno = ENOSYS };
+    -1
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn _getpid() -> c_int {
+    1
+}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _kill(_pid: c_int, _signal: c_int) -> c_int {
+    unsafe { errno = ENOSYS };
+    -1
+}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _lseek(_fd: c_int, _offset: off_t, _whence: c_int) -> off_t {
+    unsafe { errno = ENOSYS };
+    -1
+}
+
+type stat = c_void;
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _fstat(_fd: c_int, _pstat: *mut stat) -> c_int {
+    unsafe { errno = ENOSYS };
+    -1
+}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _exit(_status: c_int) -> ! {
+    unsafe { exit() }
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn _isatty(fd: c_int) -> c_int {
+    (fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO) as c_int
+}
