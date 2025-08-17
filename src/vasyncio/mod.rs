bba25d4e70a13ec0682502ccf11cd341194bbdd9
fibonacci <faisal@fibn.cc>
2025-08-18 00:02:08 +0300

    feat: add `__name__` to vasyncio

--- src/vasyncio/mod.rs
+++ src/vasyncio/mod.rs
@@ -1,5 +1,9 @@
-use micropython_rs::{const_dict, const_map, map::ConstDict};
+use micropython_rs::{const_dict, const_map, map::ConstDict, obj::Obj};
+
+use crate::qstrgen::qstr;
 
 #[unsafe(no_mangle)]
 #[allow(non_upper_case_globals)]
-static vasyncio_globals: ConstDict = const_dict![];
+static vasyncio_globals: ConstDict = const_dict![
+    qstr!(__name__) => Obj::from_qstr(qstr!(vasyncio))
+];
