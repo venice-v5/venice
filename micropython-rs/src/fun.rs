c374701f1e60fca74546e06369fa2fc6bb0718e0
fibonacci <faisal@fibn.cc>
2025-08-18 03:04:36 +0300

    refactor: make separate fun objects

--- micropython-rs/src/fun.rs
+++ micropython-rs/src/fun.rs
@@ -1,55 +1,37 @@
-use crate::obj::{Obj, ObjBase};
+use crate::obj::{Obj, ObjBase, ObjFullType, ObjType};
 
-pub type Fun0 = extern "C" fn();
-pub type Fun1 = extern "C" fn(a: Obj);
-pub type Fun2 = extern "C" fn(a: Obj, b: Obj);
-pub type Fun3 = extern "C" fn(a: Obj, b: Obj, c: Obj);
-
-mod sealed {
-    use super::{Fun0, Fun1, Fun2, Fun3};
-    use crate::obj::ObjFullType;
-
-    pub trait FunType {
-        const TYPE_OBJ: *const ObjFullType;
-    }
-
-    unsafe extern "C" {
-        static mp_type_fun_builtin_0: ObjFullType;
-        static mp_type_fun_builtin_1: ObjFullType;
-        static mp_type_fun_builtin_2: ObjFullType;
-        static mp_type_fun_builtin_3: ObjFullType;
-    }
-
-    impl FunType for Fun0 {
-        const TYPE_OBJ: *const ObjFullType = &raw const mp_type_fun_builtin_0;
-    }
-
-    impl FunType for Fun1 {
-        const TYPE_OBJ: *const ObjFullType = &raw const mp_type_fun_builtin_1;
-    }
-
-    impl FunType for Fun2 {
-        const TYPE_OBJ: *const ObjFullType = &raw const mp_type_fun_builtin_2;
-    }
-
-    impl FunType for Fun3 {
-        const TYPE_OBJ: *const ObjFullType = &raw const mp_type_fun_builtin_3;
-    }
-}
-
-use sealed::FunType;
-
-#[repr(C)]
-pub struct Fun<F: FunType> {
-    base: ObjBase,
-    fun: F,
-}
-
-impl<F: FunType> Fun<F> {
-    pub const fn new(f: F) -> Self {
-        Self {
-            base: ObjBase::new(unsafe { &*F::TYPE_OBJ }),
-            fun: f,
+macro_rules! define_fun_type {
+    ($name:ident, $fn_type:ty, $mp_type_name:ident) => {
+        #[repr(C)]
+        pub struct $name {
+            base: ObjBase,
+            fun: $fn_type,
         }
-    }
+
+        unsafe extern "C" {
+            static $mp_type_name: ObjFullType;
+        }
+
+        unsafe impl ObjType for $name {
+            const TYPE_OBJ: *const ObjFullType = &raw const $mp_type_name;
+        }
+
+        impl $name {
+            pub const fn new(f: $fn_type) -> Self {
+                Self {
+                    base: ObjBase::new(unsafe { &$mp_type_name }),
+                    fun: f,
+                }
+            }
+        }
+    };
 }
+
+define_fun_type!(Fun0, extern "C" fn() -> Obj, mp_type_fun_builtin_0);
+define_fun_type!(Fun1, extern "C" fn(Obj) -> Obj, mp_type_fun_builtin_1);
+define_fun_type!(Fun2, extern "C" fn(Obj, Obj) -> Obj, mp_type_fun_builtin_2);
+define_fun_type!(
+    Fun3,
+    extern "C" fn(Obj, Obj, Obj) -> Obj,
+    mp_type_fun_builtin_3
+);
