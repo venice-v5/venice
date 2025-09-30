c74f1deff61dc395fa2421db1899080f711b1457
fibonacci <faisal@fibn.cc>
2025-09-30 18:10:55 +0300

    refactor: remove rust_qstrdefs.h generation, add venice_qstrdefs.h

--- build.rs
+++ build.rs
@@ -1,39 +1,8 @@
 use core::writeln;
-use std::{
-    ffi::OsStr,
-    fs::{OpenOptions, ReadDir},
-    io::Write,
-    path::Path,
-    process::Command,
-};
+use std::{fs::OpenOptions, io::Write, path::Path, process::Command};
 
 use format_bytes::write_bytes;
 use regex::bytes::Regex;
-
-fn rust_qstrs_inner(re: &Regex, entries: ReadDir, vec: &mut Vec<Vec<u8>>) {
-    for entry in entries.flatten() {
-        let path = entry.path();
-        if path.extension() == Some(OsStr::new("rs")) {
-            let src = std::fs::read(path).unwrap();
-            for mat in re.captures_iter(&src) {
-                vec.push(mat[1].to_vec());
-            }
-        } else if let Ok(file_type) = entry.file_type()
-            && file_type.is_dir()
-        {
-            rust_qstrs_inner(re, std::fs::read_dir(path).unwrap(), vec);
-        }
-    }
-}
-
-fn rust_qstrs(manifest_dir: &Path) -> Vec<Vec<u8>> {
-    let re = Regex::new(r"qstr!\(([a-zA-Z0-9_]*)\)").unwrap();
-    let mut vec = Vec::new();
-
-    let src_dir = manifest_dir.join("src");
-    rust_qstrs_inner(&re, std::fs::read_dir(&src_dir).unwrap(), &mut vec);
-    vec
-}
 
 fn generated_qstrs(out_dir: &Path) -> Vec<Vec<u8>> {
     let qstrdefs_generated_h = std::fs::read(out_dir.join("genhdr/qstrdefs.generated.h")).unwrap();
@@ -62,13 +31,6 @@ fn main() {
 
     let mut open_ops = OpenOptions::new();
     open_ops.create(true).write(true).truncate(true);
-
-    let rust_qstrs = rust_qstrs(Path::new(manifest_dir));
-    let mut rust_qstrdefs_h = open_ops.open(out_dir_path.join("rust_qstrdefs.h")).unwrap();
-
-    for qstr in rust_qstrs.iter() {
-        write_bytes!(&mut rust_qstrdefs_h, b"Q({})\n", qstr).unwrap();
-    }
 
     let libmpyv5 = std::env::var("VENICE_LIBMPYV5_PATH").unwrap_or_else(|_| {
         let output = Command::new("make")
