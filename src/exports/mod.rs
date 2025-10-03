37c495ae12e12f76e38ed1c0dd6d4bc20a83cd00
fibonacci <faisal@fibn.cc>
2025-10-03 22:52:11 +0300

    fix: mark function as unsafe

--- src/exports/mod.rs
+++ src/exports/mod.rs
@@ -6,7 +6,7 @@ use core::ffi::{c_char, c_void};
 use crate::{ALLOCATOR, serial::print_bytes};
 
 #[unsafe(no_mangle)]
-extern "C" fn mp_hal_stdout_tx_strn_cooked(str: *const c_char, len: u32) {
+unsafe extern "C" fn mp_hal_stdout_tx_strn_cooked(str: *const c_char, len: u32) {
     let slice = unsafe { core::slice::from_raw_parts(str, len as usize) };
     print_bytes(slice);
 }
