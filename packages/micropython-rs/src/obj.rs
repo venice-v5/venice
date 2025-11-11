f340baf188ed9e700c0d6e8421b62a78767818e9
fibonacci <faisal@fibn.cc>
2025-11-11 17:38:19 +0300

    fix: add `'static` lifetime bound to object allocation functions

--- packages/micropython-rs/src/obj.rs
+++ packages/micropython-rs/src/obj.rs
@@ -541,7 +541,7 @@ impl Obj {
     pub const TRUE: Self = Self::from_immediate(3);
     pub const FALSE: Self = Self::from_immediate(1);
 
-    pub fn new<T: ObjTrait>(o: T, alloc: &mut Gc) -> Result<Self, GcError> {
+    pub fn new<T: ObjTrait + 'static>(o: T, alloc: &mut Gc) -> Result<Self, GcError> {
         unsafe {
             let mem = alloc.alloc(size_of::<T>());
             if mem.is_null() {
--- packages/venice/src/obj.rs
+++ packages/venice/src/obj.rs
@@ -2,6 +2,6 @@ use micropython_rs::obj::{Obj, ObjTrait};
 
 use crate::ALLOCATOR;
 
-pub fn alloc_obj<T: ObjTrait>(o: T) -> Obj {
+pub fn alloc_obj<T: ObjTrait + 'static>(o: T) -> Obj {
     Obj::new(o, ALLOCATOR.lock().as_mut().unwrap()).unwrap()
 }
