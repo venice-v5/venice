25a3679712493808307dbbdd260c03c3a102a1e9
fibonacci <faisal@fibn.cc>
2025-12-05 03:15:42 +0300

    refactor: clean up registry.rs, add `RegistryGuard::free`

--- packages/venice/src/registry.rs
+++ packages/venice/src/registry.rs
@@ -3,6 +3,7 @@ use std::{
     sync::{Mutex, MutexGuard},
 };
 
+use micropython_rs::{except::raise_value_error, init::token};
 use vexide_devices::smart::SmartPort;
 
 pub trait PortDevice {
@@ -18,16 +19,14 @@ pub struct Registry {
     device: Mutex<RegistryDevice>,
 }
 
-enum GuardDevice<D: PortDevice> {
-    Active(RefCell<D>),
-    Dropped,
+struct ActiveRegistryGuard<'a, D: PortDevice> {
+    device: D,
+    guard: MutexGuard<'a, RegistryDevice>,
 }
 
 #[must_use]
 pub struct RegistryGuard<'a, D: PortDevice> {
-    // only used in Drop implementation, should never be None in usage
-    device: GuardDevice<D>,
-    registry_device: MutexGuard<'a, RegistryDevice>,
+    guard: RefCell<Option<ActiveRegistryGuard<'a, D>>>,
 }
 
 impl RegistryDevice {
@@ -52,8 +51,10 @@ impl Registry {
             .try_lock()
             .map(|mut registry_device| match registry_device.take() {
                 RegistryDevice::Port(port) => RegistryGuard {
-                    device: GuardDevice::Active(RefCell::new(init(port))),
-                    registry_device: registry_device,
+                    guard: RefCell::new(Some(ActiveRegistryGuard {
+                        device: init(port),
+                        guard: registry_device,
+                    })),
                 },
                 RegistryDevice::Occupied => panic!("registry guard not dropped"),
             })
@@ -61,57 +62,56 @@ impl Registry {
     }
 }
 
-impl<D: PortDevice> GuardDevice<D> {
-    fn borrow<'a>(&'a self) -> Ref<'a, D> {
-        match self {
-            Self::Active(d) => d.borrow(),
-            // should never happen
-            Self::Dropped => panic!(),
-        }
-    }
-
-    fn borrow_mut<'a>(&'a self) -> RefMut<'a, D> {
-        match self {
-            Self::Active(d) => d.borrow_mut(),
-            // should never happen
-            Self::Dropped => panic!(),
-        }
-    }
-
-    fn get_mut(&mut self) -> &mut D {
-        match self {
-            Self::Active(d) => d.get_mut(),
-            // should never happen
-            Self::Dropped => panic!(),
-        }
-    }
-
-    fn drop(&mut self) -> SmartPort {
-        match std::mem::replace(self, Self::Dropped) {
-            Self::Active(d) => d.into_inner().take_port(),
-            // should never happen
-            Self::Dropped => panic!("attempt to drop GuardDevice twice"),
-        }
-    }
-}
-
 impl<'a, D: PortDevice> RegistryGuard<'a, D> {
-    pub fn borrow(&'a self) -> Ref<'a, D> {
-        self.device.borrow()
+    pub fn try_borrow<'b>(&'b self) -> Result<Ref<'b, D>, ()> {
+        Ref::filter_map(self.guard.borrow(), |guard| {
+            guard.as_ref().map(|guard| &guard.device)
+        })
+        .map_err(|_| ())
     }
 
-    pub fn borrow_mut(&'a self) -> RefMut<'a, D> {
-        self.device.borrow_mut()
+    pub fn try_borrow_mut<'b>(&'b self) -> Result<RefMut<'b, D>, ()> {
+        RefMut::filter_map(self.guard.borrow_mut(), |guard| {
+            guard.as_mut().map(|guard| &mut guard.device)
+        })
+        .map_err(|_| ())
     }
 
-    pub fn get_mut(&mut self) -> &mut D {
-        self.device.get_mut()
+    pub fn borrow<'b>(&'b self) -> Ref<'b, D> {
+        self.try_borrow().unwrap_or_else(|_| {
+            raise_value_error(token().unwrap(), "attempt to use device after free")
+        })
+    }
+
+    pub fn borrow_mut<'b>(&'b self) -> RefMut<'b, D> {
+        self.try_borrow_mut().unwrap_or_else(|_| {
+            raise_value_error(token().unwrap(), "attempt to use device after free")
+        })
+    }
+
+    pub fn free(&self) -> Result<(), ()> {
+        let guard = self.guard.replace(None);
+        match guard {
+            Some(mut guard) => {
+                *guard.guard = RegistryDevice::Port(guard.device.take_port());
+                Ok(())
+            }
+            None => Err(()),
+        }
+    }
+
+    pub fn free_or_raise(&self) {
+        self.free()
+            .unwrap_or_else(|_| raise_value_error(token().unwrap(), "attempt to free device twice"))
     }
 }
 
 impl<'a, D: PortDevice> Drop for RegistryGuard<'a, D> {
     fn drop(&mut self) {
-        *self.registry_device = RegistryDevice::Port(self.device.drop())
+        let guard = std::mem::replace(self.guard.get_mut(), None);
+        if let Some(mut guard) = guard {
+            *guard.guard = RegistryDevice::Port(guard.device.take_port());
+        }
     }
 }
 
