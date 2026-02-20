579c4a83972109c86047d85e37a870f0a4533c52
fibonacci <faisal@fibn.cc>
2026-02-20 13:24:48 +0300

    chore: suppress c port type warnings

--- packages/venice/port/modvasyncio.c
+++ packages/venice/port/modvasyncio.c
@@ -4,7 +4,7 @@ extern const mp_obj_dict_t vasyncio_globals;
 
 const mp_obj_module_t vasyncio_module = {
     .base = { &mp_type_module },
-    .globals = &vasyncio_globals,
+    .globals = (mp_obj_dict_t*)&vasyncio_globals,
 };
 
 MP_REGISTER_MODULE(MP_QSTR_vasyncio, vasyncio_module);
--- packages/venice/port/modvenice.c
+++ packages/venice/port/modvenice.c
@@ -4,7 +4,7 @@ extern const mp_obj_dict_t venice_globals;
 
 const mp_obj_module_t venice_module = {
     .base = { &mp_type_module },
-    .globals = &venice_globals,
+    .globals = (mp_obj_dict_t*)&venice_globals,
 };
 
 MP_REGISTER_MODULE(MP_QSTR_venice, venice_module);
