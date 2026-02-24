491c32e5e4b34ef407ae7029e9afc90f960054c2
fibonacci <faisal@fibn.cc>
2026-02-24 06:38:48 +0300

    fix: AiVisionObject attr + add coercion

--- packages/venice/src/modvenice/ai_vision/ai_vision_object.rs
+++ packages/venice/src/modvenice/ai_vision/ai_vision_object.rs
@@ -52,6 +52,13 @@ pub struct AiVisionObjectObj {
 }
 unsafe impl ObjTrait for AiVisionObjectObj {
     const OBJ_TYPE: &ObjType = AI_VISION_OBJECT_OBJ_TYPE.as_obj_type();
+
+    fn coercable(ty: &ObjType) -> bool {
+        ty == AI_VISION_COLOR_OBJECT_OBJ_TYPE.as_obj_type()
+            || ty == AI_VISION_CODE_OBJECT_OBJ_TYPE.as_obj_type()
+            || ty == AI_VISION_APRIL_TAG_OBJECT_OBJ_TYPE.as_obj_type()
+            || ty == AI_VISION_MODEL_OBJECT_OBJ_TYPE.as_obj_type()
+    }
 }
 impl AiVisionObjectObj {
     pub fn create_obj(object: AiVisionObject) -> Obj {
@@ -91,96 +98,78 @@ fn ai_vision_object_state_attr(this: &AiVisionObjectObj, attr: Qstr, op: AttrOp)
         }
         "position_x" => {
             let pos = match this.object {
-                AiVisionObject::Color { position, .. } => Some(position.x),
-                AiVisionObject::Code { position, .. } => Some(position.x),
-                AiVisionObject::AprilTag { .. } => None,
-                AiVisionObject::Model { position, .. } => Some(position.x),
+                AiVisionObject::Color { position, .. } => position.x,
+                AiVisionObject::Code { position, .. } => position.x,
+                AiVisionObject::Model { position, .. } => position.x,
+                AiVisionObject::AprilTag { .. } => return,
             };
-            if let Some(v) = pos {
-                Obj::from_int(v as _)
-            } else {
-                Obj::NONE
-            }
+            Obj::from_int(pos as _)
         }
         "position_y" => {
             let pos = match this.object {
-                AiVisionObject::Color { position, .. } => Some(position.y),
-                AiVisionObject::Code { position, .. } => Some(position.y),
-                AiVisionObject::AprilTag { .. } => None,
-                AiVisionObject::Model { position, .. } => Some(position.y),
+                AiVisionObject::Color { position, .. } => position.y,
+                AiVisionObject::Code { position, .. } => position.y,
+                AiVisionObject::Model { position, .. } => position.y,
+                AiVisionObject::AprilTag { .. } => return,
             };
-            if let Some(v) = pos {
-                Obj::from_int(v as _)
-            } else {
-                Obj::NONE
-            }
+            Obj::from_int(pos as _)
         }
         "width" => {
             let width = match this.object {
-                AiVisionObject::Color { width, .. } => Some(width),
-                AiVisionObject::Code { width, .. } => Some(width),
-                AiVisionObject::Model { width, .. } => Some(width),
-                _ => None,
+                AiVisionObject::Color { width, .. } => width,
+                AiVisionObject::Code { width, .. } => width,
+                AiVisionObject::Model { width, .. } => width,
+                _ => return,
             };
-            if let Some(w) = width {
-                Obj::from_int(w as _)
-            } else {
-                Obj::NONE
-            }
+            Obj::from_int(width as _)
         }
         "height" => {
             let height = match this.object {
-                AiVisionObject::Color { height, .. } => Some(height),
-                AiVisionObject::Code { height, .. } => Some(height),
-                AiVisionObject::Model { height, .. } => Some(height),
-                _ => None,
+                AiVisionObject::Color { height, .. } => height,
+                AiVisionObject::Code { height, .. } => height,
+                AiVisionObject::Model { height, .. } => height,
+                _ => return,
             };
-            if let Some(h) = height {
-                Obj::from_int(h as _)
-            } else {
-                Obj::NONE
-            }
+            Obj::from_int(height as _)
         }
         "classification" => {
-            if let AiVisionObject::Model { classification, .. } = &this.object {
-                Obj::from_qstr(Qstr::from_str(classification.as_str()))
-            } else {
-                Obj::NONE
-            }
+            let AiVisionObject::Model { classification, .. } = &this.object else {
+                return;
+            };
+            Obj::from_qstr(Qstr::from_str(classification.as_str()))
         }
         "confidence" => {
-            if let AiVisionObject::Model { confidence, .. } = this.object {
-                Obj::from_int(confidence as _)
-            } else {
-                Obj::NONE
-            }
+            let AiVisionObject::Model { confidence, .. } = this.object else {
+                return;
+            };
+            Obj::from_int(confidence as _)
         }
         // Code's angle is implemented as a method
         _ => {
-            if let AiVisionObject::AprilTag {
+            let AiVisionObject::AprilTag {
                 top_left,
                 top_right,
                 bottom_left,
                 bottom_right,
                 ..
             } = this.object
-            {
-                let value = match attr.as_str() {
-                    "top_left_x" => top_left.x,
-                    "top_left_y" => top_left.y,
-                    "top_right_x" => top_right.x,
-                    "top_right_y" => top_right.y,
-                    "bottom_left_x" => bottom_left.x,
-                    "bottom_left_y" => bottom_left.y,
-                    "bottom_right_x" => bottom_right.x,
-                    "bottom_right_y" => bottom_right.y,
-                    _ => return,
-                };
+            else {
+                return;
+            };
 
-                Obj::from_int(value as _)
-            } else {
-                Obj::NONE
-            }
+            let value = match attr.as_str() {
+                "top_left_x" => top_left.x,
+                "top_left_y" => top_left.y,
+                "top_right_x" => top_right.x,
+                "top_right_y" => top_right.y,
+                "bottom_left_x" => bottom_left.x,
+                "bottom_left_y" => bottom_left.y,
+                "bottom_right_x" => bottom_right.x,
+                "bottom_right_y" => bottom_right.y,
+                _ => return,
+            };
+
+            Obj::from_int(value as _)
         }
     });
 }
