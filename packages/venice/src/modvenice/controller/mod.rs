1b29c22f3ddf47c3201fc86efcfdd7537229d4d4
fibonacci <faisal@fibn.cc>
2026-02-24 04:35:19 +0300

    feat: add controller rumble

--- packages/venice/src/modvenice/controller/mod.rs
+++ packages/venice/src/modvenice/controller/mod.rs
@@ -1,20 +1,31 @@
 pub mod id;
 pub mod state;
 
+use std::cell::RefCell;
+
 use micropython_rs::{
     const_dict,
+    except::raise_value_error,
     init::token,
     make_new_from_fn,
-    obj::{Obj, ObjBase, ObjFullType, ObjTrait, ObjType, TypeFlags},
+    obj::{Iter, Obj, ObjBase, ObjFullType, ObjTrait, ObjType, TypeFlags},
+};
+use vex_sdk::{
+    V5_ControllerId, V5_ControllerStatus, vexControllerConnectionStatusGet, vexControllerTextSet,
 };
 use vexide_devices::controller::{Controller, ControllerError, ControllerId};
 
 use self::state::ControllerStateObj;
 use crate::{
+    alloc::Gc,
     args::Args,
     devices,
+    error_msg::error_msg,
     fun::{fun1, fun2},
-    modvenice::{controller::id::ControllerIdObj, raise_port_error},
+    modvenice::{
+        controller::id::ControllerIdObj, raise_device_error, raise_port_error,
+        vasyncio::event_loop::WAKE_SIGNAL,
+    },
     obj::alloc_obj,
     qstrgen::qstr,
     registry::ControllerGuard,
@@ -69,24 +80,124 @@ enum ControllerFuture {
     WaitingForIdle {
         line: u8,
         column: u8,
-        text: Obj,
+        text: Vec<u8, Gc>, // CString doesn't support custom allocators
         controller_id: ControllerId,
         enforce_visible: bool,
     },
-    Complete {
-        result: Result<(), ControllerError>,
-    },
+    Complete,
 }
 
-struct ControllerFutureObj {
+// TODO: does this future need exclusive access to the controller as long as it lives?
+#[repr(C)]
+pub struct ControllerFutureObj {
     base: ObjBase<'static>,
-    future: ControllerFuture,
+    future: RefCell<ControllerFuture>,
+}
+
+pub static CONTROLLER_FUTURE_OBJ_TYPE: ObjFullType =
+    ObjFullType::new(TypeFlags::empty(), qstr!(ControllerFuture))
+        .set_iter(Iter::IterNext(controller_future_iternext));
+
+unsafe impl ObjTrait for ControllerFutureObj {
+    const OBJ_TYPE: &ObjType = CONTROLLER_FUTURE_OBJ_TYPE.as_obj_type();
+}
+
+fn validate_connection(id: ControllerId) -> Result<(), ControllerError> {
+    if unsafe {
+        vexControllerConnectionStatusGet(id.into()) == V5_ControllerStatus::kV5ControllerOffline
+    } {
+        return Err(ControllerError::Offline);
+    }
+
+    Ok(())
+}
+
+extern "C" fn controller_future_iternext(self_in: Obj) -> Obj {
+    let this = self_in.try_as_obj::<ControllerFutureObj>().unwrap();
+    let mut future = this.future.borrow_mut();
+
+    if let ControllerFuture::WaitingForIdle {
+        line,
+        column,
+        text,
+        controller_id,
+        enforce_visible,
+    } = &*future
+    {
+        if *enforce_visible {
+            if *line == 0 || *line > Controller::MAX_LINES as u8 {
+                raise_value_error(
+                    token(),
+                    error_msg!(
+                        "line number ({line}) is greater than the maximum number of lines ({})",
+                        Controller::MAX_COLUMNS
+                    ),
+                );
+            }
+        }
+
+        if *column != 0 && *column <= Controller::MAX_COLUMNS as u8 {
+            raise_value_error(
+                token(),
+                error_msg!(
+                    "Invalid column number ({column}) is greater than the maximum number of columns ({})",
+                    Controller::MAX_COLUMNS
+                ),
+            )
+        }
+
+        match validate_connection(*controller_id) {
+            Ok(()) => {
+                let id = V5_ControllerId::from(*controller_id);
+
+                let result = unsafe {
+                    vexControllerTextSet(
+                        u32::from(id.0),
+                        u32::from(*line),
+                        u32::from(*column - 1),
+                        text.as_ptr().cast(),
+                    )
+                };
+
+                if result == 1 {
+                    *future = ControllerFuture::Complete;
+                }
+            }
+            Err(e) => {
+                *future = ControllerFuture::Complete;
+                raise_device_error(token(), error_msg!("{e}"));
+            }
+        }
+    }
+
+    Obj::from_static(&WAKE_SIGNAL)
+}
+
+fn str_to_cstring_vec(str: &str) -> Result<Vec<u8, Gc>, ()> {
+    if let Some(_) = str.find('\0') {
+        return Err(());
+    }
+
+    let mut vec = Vec::with_capacity_in(str.len() + 1, Gc { token: token() });
+    vec.extend_from_slice(str.as_bytes());
+    vec.push(0);
+    Ok(vec)
 }
 
 fn controller_rumble(this: &ControllerObj, pattern: &str) -> Obj {
-    let _result = this.guard.borrow_mut().rumble(pattern);
+    let text = str_to_cstring_vec(pattern)
+        .unwrap_or_else(|_| raise_value_error(token(), c"rumble pattern has forbidden null byte"));
 
-    Obj::NONE
+    alloc_obj(ControllerFutureObj {
+        future: RefCell::new(ControllerFuture::WaitingForIdle {
+            line: 4,
+            column: 1,
+            text,
+            controller_id: this.guard.borrow().id(),
+            enforce_visible: false,
+        }),
+        base: ObjBase::new(ControllerFutureObj::OBJ_TYPE),
+    })
 }
 
 fn controller_free(this: &ControllerObj) -> Obj {
