2096d5e0ecd390b0b785ad96f69581bc6762f4f3
fibonacci <faisal@fibn.cc>
2026-03-08 14:52:08 +0300

    refactor: make #[constant] name optional

--- packages/micropython-macros/src/constants.rs
+++ packages/micropython-macros/src/constants.rs
@@ -1,17 +1,22 @@
 use proc_macro2::TokenStream;
 use quote::{quote, quote_spanned};
 use syn::{
-    Attribute, ExprMacro, ImplItemConst, Type,
+    Attribute, ExprMacro, ImplItemConst, Meta, Type,
     parse::{Parse, ParseStream},
     spanned::Spanned,
 };
 
+#[derive(Default)]
 pub struct ConstantArgs {
-    pub qstr_macro: ExprMacro,
+    pub qstr_macro: Option<ExprMacro>,
 }
 
 impl Parse for ConstantArgs {
     fn parse(input: ParseStream) -> syn::Result<Self> {
+        if input.is_empty() {
+            return Ok(Self::default());
+        }
+
         let qstr_macro = input.parse::<ExprMacro>()?;
         if !qstr_macro.mac.path.is_ident("qstr") {
             return Err(syn::Error::new_spanned(
@@ -20,7 +25,9 @@ impl Parse for ConstantArgs {
             ));
         }
 
-        Ok(Self { qstr_macro })
+        Ok(Self {
+            qstr_macro: Some(qstr_macro),
+        })
     }
 }
 
@@ -29,7 +36,11 @@ pub fn generate_constant(
     constant: ImplItemConst,
     attr: Attribute,
 ) -> syn::Result<TokenStream> {
-    let args = attr.parse_args::<ConstantArgs>()?;
+    let args = match &attr.meta {
+        Meta::Path(_) => ConstantArgs::default(),
+        Meta::List(_) => attr.parse_args()?,
+        Meta::NameValue(_) => return Err(syn::Error::new(attr.span(), "unsupported `=` format")),
+    };
 
     let constructor = if let Type::Path(path) = &constant.ty
         && let Some(ident) = path.path.get_ident()
@@ -47,9 +58,19 @@ pub fn generate_constant(
         None
     };
 
-    let qstr = &args.qstr_macro;
     let spanned_ty = quote_spanned! (constant.span()=> <#ty>);
+
     let constant_name = &constant.ident;
+    let qstr = match &args.qstr_macro {
+        Some(m) => {
+            let mac = &m.mac;
+            quote! { #mac }
+        }
+        None => {
+            quote! { qstr!(#constant_name) }
+        }
+    };
+
     Ok(match constructor {
         Some(cons) => {
             quote! { #qstr => ::micropython_rs::obj::Obj::#cons(#spanned_ty::#constant_name), }
