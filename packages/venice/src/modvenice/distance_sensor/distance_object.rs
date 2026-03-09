b65db768c0ef2ebc28a19efbece841a1dd09ce0f
fibonacci <faisal@fibn.cc>
2026-03-09 17:21:50 +0300

    feat: rewrite distance sensor using macro

--- packages/venice/src/modvenice/distance_sensor/distance_object.rs
+++ packages/venice/src/modvenice/distance_sensor/distance_object.rs
@@ -1,24 +1,17 @@
 use micropython_rs::{
-    attr_from_fn,
-    obj::{AttrOp, Obj, ObjBase, ObjFullType, ObjTrait, ObjType, TypeFlags},
+    class, class_methods,
+    obj::{AttrOp, Obj, ObjBase, ObjTrait},
     qstr::Qstr,
 };
 use vexide_devices::smart::distance::DistanceObject;
 
 use crate::qstrgen::qstr;
 
+#[class(qstr!(DistanceObject))]
 #[repr(C)]
 pub struct DistanceObjectObj {
     base: ObjBase<'static>,
     object: DistanceObject,
-}
-
-pub static DISTANCE_OBJECT_OBJ_TYPE: ObjFullType =
-    ObjFullType::new(TypeFlags::empty(), qstr!(DistanceObject))
-        .set_attr(attr_from_fn!(distance_object_attr));
-
-unsafe impl ObjTrait for DistanceObjectObj {
-    const OBJ_TYPE: &ObjType = DISTANCE_OBJECT_OBJ_TYPE.as_obj_type();
 }
 
 impl DistanceObjectObj {
@@ -30,13 +23,17 @@ impl DistanceObjectObj {
     }
 }
 
-fn distance_object_attr(this: &DistanceObjectObj, attr: Qstr, op: AttrOp) {
-    let AttrOp::Load { result } = op else { return };
-    result.return_value(match attr.as_str() {
-        "confidence" => Obj::from_float(this.object.confidence as _),
-        "distance" => Obj::from_int(this.object.distance as _),
-        "relative_size" => Obj::from_int(this.object.relative_size as _),
-        "velocity" => Obj::from_float(this.object.velocity as _),
-        _ => return,
-    });
+#[class_methods]
+impl DistanceObjectObj {
+    #[attr]
+    fn attr(&self, attr: Qstr, op: AttrOp) {
+        let AttrOp::Load { result } = op else { return };
+        result.return_value(match attr.as_str() {
+            "confidence" => Obj::from_float(self.object.confidence as _),
+            "distance" => Obj::from_int(self.object.distance as _),
+            "relative_size" => Obj::from_int(self.object.relative_size as _),
+            "velocity" => Obj::from_float(self.object.velocity as _),
+            _ => return,
+        });
+    }
 }
--- packages/venice/src/modvenice/distance_sensor/mod.rs
+++ packages/venice/src/modvenice/distance_sensor/mod.rs
@@ -2,80 +2,65 @@ pub mod distance_object;
 
 use argparse::Args;
 use micropython_rs::{
-    const_dict,
+    class, class_methods,
     except::raise_value_error,
     init::token,
-    make_new_from_fn,
-    obj::{Obj, ObjBase, ObjFullType, ObjTrait, ObjType, TypeFlags},
+    obj::{Obj, ObjBase, ObjType},
 };
 use vexide_devices::smart::distance::DistanceSensor;
 
 use crate::{
     devices::{self, PortNumber},
-    fun::fun1,
     modvenice::{distance_sensor::distance_object::DistanceObjectObj, raise_port_error},
-    obj::alloc_obj,
     qstrgen::qstr,
     registry::RegistryGuard,
 };
 
+#[class(qstr!(DistanceSensor))]
 #[repr(C)]
 pub struct DistanceSensorObj {
     base: ObjBase<'static>,
     guard: RegistryGuard<'static, DistanceSensor>,
 }
 
-pub static DISTANCE_SENSOR_OBJ_TYPE: ObjFullType =
-    ObjFullType::new(TypeFlags::empty(), qstr!(DistanceSensor))
-        .set_make_new(make_new_from_fn!(distance_sensor_make_new))
-        .set_locals_dict(const_dict![
-            qstr!(object) => Obj::from_static(&fun1!(distance_sensor_object, &DistanceSensorObj)),
-            qstr!(status) => Obj::from_static(&fun1!(distance_sensor_status, &DistanceSensorObj)),
-            qstr!(free) => Obj::from_static(&fun1!(distance_sensor_free, &DistanceSensorObj)),
-        ]);
+#[class_methods]
+impl DistanceSensorObj {
+    #[make_new]
+    fn make_new(ty: &'static ObjType, n_pos: usize, n_kw: usize, args: &[Obj]) -> Self {
+        let token = token();
+        let mut reader = Args::new(n_pos, n_kw, args).reader(token);
+        reader.assert_npos(1, 1).assert_nkw(0, 0);
 
-unsafe impl ObjTrait for DistanceSensorObj {
-    const OBJ_TYPE: &ObjType = DISTANCE_SENSOR_OBJ_TYPE.as_obj_type();
-}
+        let port = PortNumber::from_i32(reader.next_positional())
+            .unwrap_or_else(|_| raise_value_error(token, c"port number must be between 1 and 21"));
 
-fn distance_sensor_make_new(ty: &'static ObjType, n_pos: usize, n_kw: usize, args: &[Obj]) -> Obj {
-    let token = token();
-    let mut reader = Args::new(n_pos, n_kw, args).reader(token);
-    reader.assert_npos(1, 1).assert_nkw(0, 0);
+        let guard = devices::lock_port(port, DistanceSensor::new);
 
-    let port = PortNumber::from_i32(reader.next_positional())
-        .unwrap_or_else(|_| raise_value_error(token, c"port number must be between 1 and 21"));
-
-    let guard = devices::lock_port(port, DistanceSensor::new);
-
-    alloc_obj(DistanceSensorObj {
-        base: ObjBase::new(ty),
-        guard,
-    })
-}
-
-fn distance_sensor_status(this: &DistanceSensorObj) -> Obj {
-    let status = this
-        .guard
-        .borrow()
-        .status()
-        .unwrap_or_else(|e| raise_port_error!(e));
-    Obj::from_int(status as i32)
-}
-
-fn distance_sensor_object(this: &DistanceSensorObj) -> Obj {
-    let status = this
-        .guard
-        .borrow()
-        .object()
-        .unwrap_or_else(|e| raise_port_error!(e));
-    match status {
-        Some(state) => alloc_obj(DistanceObjectObj::new(state)),
-        None => Obj::NONE,
+        DistanceSensorObj {
+            base: ObjBase::new(ty),
+            guard,
+        }
     }
-}
 
-fn distance_sensor_free(this: &DistanceSensorObj) -> Obj {
-    this.guard.free_or_raise();
-    Obj::NONE
+    #[method]
+    fn status(&self) -> i32 {
+        self.guard
+            .borrow()
+            .status()
+            .unwrap_or_else(|e| raise_port_error!(e)) as i32
+    }
+
+    #[method]
+    fn object(&self) -> Option<DistanceObjectObj> {
+        self.guard
+            .borrow()
+            .object()
+            .unwrap_or_else(|e| raise_port_error!(e))
+            .map(DistanceObjectObj::new)
+    }
+
+    #[method]
+    fn free(&self) {
+        self.guard.free_or_raise();
+    }
 }
