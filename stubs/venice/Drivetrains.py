b01040880ec548d29d88bff12bcbdf68e1fbe98e
ForgotenMail <144358054+ForgotenMail@users.noreply.github.com>
2026-03-09 10:22:11 -0700

    Update Drivetrains.py

--- stubs/venice/Drivetrains.py
+++ stubs/venice/Drivetrains.py
@@ -1,90 +1,97 @@
-from venice import Motor, Gearset, Direction
+from venice import Direction, Gearset, Motor
+
 """This file is where you will define your drivetrain
-This varible down here will define what drivetrain you will use,
-The options for drive trains right now are:
-    Tank Drive
-    In the future we will add:
-    Mechanum
-    Tank Drive 2 Motor
-    X-Drive
+The variable DriveTrainType is what defines what dirve train you have, the current drivetrains that are
+supported are these (Name followed for the variable in paranthesis)
+Tank/Differential any motors (tank)
+X - drive 4 Motors (holomonic)
+Mecanum 4 motors (holomonic)
+The math behind X drive and mecanum 4 motors is the same so we just call them the same thing! """
 
-    Copy and paste the name of your drive train from the options into the variable
-    Names of drivetrains:
-        tank
-        x_drive
-        mechanum
-
-    Ex:
-    DriveTrainType = "tank" """
 DriveTrainType = "Null"
 
-"""Type how many motors you have in these lists, it goes from front to back!
-Only 11 wats thouugh"""
+"""Type how many motors you have in these lists, it goes from front to back! """
 LeftMotors: list = [0, 0]
 RightMotors: list = [0, 0]
 
-"""You are done! Keep reading if you are curious how the code functions!"""
+# The way we define drivetrains is that each type of drivetrain is a class
+# At the end, we declare our variable called "drivetrain" to be one of these classes
+#   That depends on what the user puts in
+# Each drivetrain contaitns the logic on how to move given Forward velocity, and how much you want to turn
+
 
 def create_motor(port: int) -> Motor:
-
-    direction = Direction.FORWARD
     port_abs: int = abs(port)
-
     if port < 0:
         return Motor(port_abs, Direction.REVERSE, Gearset.GREEN)
     else:
         return Motor(port_abs, Direction.FORWARD, Gearset.GREEN)
 
-def TankDrive(LeftMotors, RightMotors):
-    LeftMotorsList: list = []
 
-    for motors in LeftMotors:
-        LeftMotorsList.append(create_motor(LeftMotors[motors]))
+class TankDrivetrain:
+    def __init__(self, left_ports: list[int], right_ports: list[int]):
+        self.left_motors = [create_motor(p) for p in left_ports]
+        self.right_motors = [create_motor(p) for p in right_ports]
 
-
-        RightMotorsList: list = []
-
-        for motors in RightMotors:
-            RightMotorsList.append(create_motor(RightMotors[motors]))
-
-    return LeftMotors, RightMotors
-    def MoveForward(LeftMotors, RightMotors, Velocity)
-        NegativeVeloctiy = Velocity*-1
-        for motor in LeftMotors:
-            motor.set_voltage(Velocity)
-        for motor in RightMotors:
-            motor.set_voltage(Velocity)
+    def move(self, velocity, turn: int):
+        # These for loops just say for each motor on each side, move the motor however fast forward
+        # and how fast you want to turn. This means regardless of our drivetrain type,
+        #  we can call the same function!
+        for m in self.left_motors:
+            m.set_voltage(velocity + turn)
+        for m in self.right_motors:
+            m.set_voltage(velocity - turn)
 
 
-    def Turn(LetMotors, RightMotors, Velocity)
-        for motor in LeftMotors:
-            motor.set_voltage(Velocity)
-        for motor in RightMotors:
-            motor.set_velocity(NegativeVelocity)
+class HolomonicDrive:
+    def __init__(self, left_ports: list[int], right_ports: list[int]):
+        self.LeftFront = create_motor(left_ports[0])
+        self.LeftBack = create_motor(left_ports[1])
+        self.RightFront = create_motor(right_ports[0])
+        self.RightBack = create_motor(right_ports[1])
+
+    def move(self, Forward, Lateral, Rotate):
+        FL = max(-12, min(12, Forward + Lateral + Rotate))
+        FR = max(-12, min(12, Forward - Lateral - Rotate))
+        BL = max(-12, min(12, Forward - Lateral + Rotate))
+        BR = max(-12, min(12, Forward + Lateral - Rotate))
+
+        self.LeftFront.set_voltage(FL)
+        self.RightFront.set_voltage(FR)
+        self.LeftBack.set_voltage(BL)
+        self.RightBack.set_voltage(BR)
 
 
-    def SLIGHT_TURN(LeftMotors, RightMotors, Velocity)
-        for motor in LeftMotors:
-            if 12 + Velocity > 12:
-                motor.set_voltage(12)
-            else:
-                motor.set_voltage(Velocity + 12)
-        for motor in RightMotors:
-            for motor in LeftMotors:
-                if 12 - Velocity > 12:
-                    motor.set_voltage(12)
-                else:
-                    motor.set_voltage(Velocity - 12)
+class HolomonicDrive6:
+    def __init__(self, left_ports: list[int], right_ports: list[int]):
+        self.LeftFront = create_motor(left_ports[0])
+        self.LeftMiddle = create_motor(left_ports[1])
+        self.LeftBack = create_motor(left_ports[2])
+        self.RightFront = create_motor(right_ports[0])
+        self.RightMiddle = create_motor(right_ports[1])
+        self.RightBack = create_motor(right_ports[2])
+
+    def move(self, Forward, Lateral, Rotate):
+        FL = max(-12, min(12, Forward + Lateral + Rotate))
+        FR = max(-12, min(12, Forward - Lateral - Rotate))
+        ML = max(-12, min(12, Forward + Rotate))
+        MR = max(-12, min(12, Forward - Rotate))
+        BL = max(-12, min(12, Forward - Lateral + Rotate))
+        BR = max(-12, min(12, Forward + Lateral - Rotate))
+
+        self.LeftFront.set_voltage(FL)
+        self.RightFront.set_voltage(FR)
+        self.LeftMiddle.set_voltage(ML)
+        self.RightMiddle.set_voltage(MR)
+        self.LeftBack.set_voltage(BL)
+        self.RightBack.set_voltage(BR)
 
 
-
-
-
-
-
-
-def DriveTrainSelector(LeftMotors, RightMotors):
-    if DriveTrainType == "tank":
-        TankDrive(LeftMotors, RightMotors)
-    else:
-        print("ded")
+if DriveTrainType == "tank":
+    drivetrain = TankDrivetrain(LeftMotors, RightMotors)
+elif DriveTrainType == "holomonic":
+    drivetrain = HolomonicDrive(LeftMotors, RightMotors)
+elif DriveTrainType == "holomonic6":
+    drivetrain = HolomonicDrive6(LeftMotors, RightMotors)
+else:
+    print("ded")
