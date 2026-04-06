fb37203117473389ce9f0ac2b2d8b1d208ed49f2
fibonacci <faisal@fibn.cc>
2026-04-06 23:54:39 +0300

    feat: add default Color instances (e.g. Color.WHITE), make Color immutable

--- packages/venice/src/modvenice/color.rs
+++ packages/venice/src/modvenice/color.rs
@@ -1,11 +1,8 @@
-use std::cell::Cell;
-
-use argparse::{ArgParser, Args, IntParser, ParseError, error_msg, type_name};
+use argparse::Args;
 use micropython_macros::{class, class_methods};
 use micropython_rs::{
-    except::{type_error, value_error},
-    init::token,
-    obj::{AttrOp, Obj, ObjBase, ObjType},
+    self,
+    obj::{AttrOp, Obj, ObjBase, ObjTrait, ObjType},
     qstr::Qstr,
 };
 use vexide_devices::color::Color;
@@ -16,13 +13,51 @@ use crate::modvenice::Exception;
 #[repr(C)]
 pub struct ColorObj {
     base: ObjBase,
-    r: Cell<u8>,
-    g: Cell<u8>,
-    b: Cell<u8>,
+    color: Color,
 }
 
 #[class_methods]
 impl ColorObj {
+    const fn new(color: Color) -> Self {
+        Self {
+            base: ObjBase::new(Self::OBJ_TYPE),
+            color,
+        }
+    }
+
+    #[constant]
+    pub const WHITE: &Self = &Self::new(Color::WHITE);
+    #[constant]
+    pub const SILVER: &Self = &Self::new(Color::SILVER);
+    #[constant]
+    pub const GRAY: &Self = &Self::new(Color::GRAY);
+    #[constant]
+    pub const BLACK: &Self = &Self::new(Color::BLACK);
+    #[constant]
+    pub const RED: &Self = &Self::new(Color::RED);
+    #[constant]
+    pub const MAROON: &Self = &Self::new(Color::MAROON);
+    #[constant]
+    pub const YELLOW: &Self = &Self::new(Color::YELLOW);
+    #[constant]
+    pub const OLIVE: &Self = &Self::new(Color::OLIVE);
+    #[constant]
+    pub const LIME: &Self = &Self::new(Color::LIME);
+    #[constant]
+    pub const GREEN: &Self = &Self::new(Color::GREEN);
+    #[constant]
+    pub const AQUA: &Self = &Self::new(Color::AQUA);
+    #[constant]
+    pub const TEAL: &Self = &Self::new(Color::TEAL);
+    #[constant]
+    pub const BLUE: &Self = &Self::new(Color::BLUE);
+    #[constant]
+    pub const NAVY: &Self = &Self::new(Color::NAVY);
+    #[constant]
+    pub const FUCHSIA: &Self = &Self::new(Color::FUCHSIA);
+    #[constant]
+    pub const PURPLE: &Self = &Self::new(Color::PURPLE);
+
     #[make_new]
     fn make_new(
         ty: &'static ObjType,
@@ -39,53 +74,27 @@ impl ColorObj {
 
         Ok(Self {
             base: ty.into(),
-            r: r.into(),
-            g: g.into(),
-            b: b.into(),
+            color: Color::new(r, g, b),
         })
     }
 
     #[attr]
     fn attr(&self, attr: Qstr, op: AttrOp) {
-        let channel = match attr.as_str() {
-            "r" => &self.r,
-            "g" => &self.g,
-            "b" => &self.b,
+        let AttrOp::Load { result } = op else { return };
+        result.return_value(match attr.as_str() {
+            "r" => self.color.r as i32,
+            "g" => self.color.g as i32,
+            "b" => self.color.b as i32,
             _ => return,
-        };
-
-        match op {
-            AttrOp::Load { result } => result.return_value(channel.get() as i32),
-            AttrOp::Store { src, result } => {
-                let parser = IntParser::<u8>::default();
-                match parser.parse(&src) {
-                    Ok(v) => {
-                        channel.set(v);
-                        result.success();
-                    }
-                    Err(e) => match e {
-                        ParseError::TypeError { expected } => type_error(error_msg!(
-                            "expected '{expected}', found '{}'",
-                            type_name(&src)
-                        )),
-                        ParseError::ValueError { mk_msg } => value_error(mk_msg("h")),
-                    }
-                    .raise(token()),
-                }
-            }
-            AttrOp::Delete { result } => {
-                channel.set(0);
-                result.sucess();
-            }
-        }
+        });
     }
 
     pub fn color(&self) -> Color {
-        Color::new(self.r.get(), self.g.get(), self.b.get())
+        self.color
     }
 
     #[method]
     fn as_int(&self) -> i32 {
-        self.color().into_raw() as i32
+        self.color.into_raw() as i32
     }
 }
